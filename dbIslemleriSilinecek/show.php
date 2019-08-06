e4e8bb8d3a138d63e55bd17cf1b62cff4e74dad4
NurAtc <hnuratici95@gmail.com>
2019-08-06 22:55:28 +0300

    remove dbIslemleriSilinecek

--- dbIslemleriSilinecek/show.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("db.php");
-$SQL = "SELECT id, adi, soyadi, sehir FROM db_deneme_rehber WHERE id={$_GET["id"]}";
-$rows = mysqli_query($db, $SQL);
-$row = mysqli_fetch_assoc($rows);
-echo "<h1>{$row["adi"]} {$row["soyadi"]}</h1>Adi: {$row["adi"]}<br />Soyadi: {$row["soyadi"]}<br />Sehir: {$row["sehir"]}";
-?>
