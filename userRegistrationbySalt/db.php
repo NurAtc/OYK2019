854d7489f1d15ec6a9c9ebaca02cb0741049bce8
NurAtc <53109743+NurAtc@users.noreply.github.com>
2019-08-12 00:13:46 +0300

    Update db.php

--- userRegistrationbySalt/db.php
+++ userRegistrationbySalt/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "bilgiler";
+$username = "username";
+$password = "password";
+$dbname = "dbname";
 
 $db = mysqli_connect($servername, $username, $password, $dbname);
 if(!$db) die("Error!" . mysqli_connect_error()); 
