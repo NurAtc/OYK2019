023f97c6b58e2ad52079673300107b005853b8c7
NurAtc <53109743+NurAtc@users.noreply.github.com>
2019-08-12 00:14:58 +0300

    Update db.php

--- insertandListTwoNumbers_Db/db.php
+++ insertandListTwoNumbers_Db/db.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "bilgiler";
+$username = "username";
+$password = "password";
+$dbname = "databasename";
 
 $db = mysqli_connect($servername, $username, $password, $dbname);
 if(!$db) die("Hata olustu!" . mysqli_connect_error());
